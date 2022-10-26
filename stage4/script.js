e0048d79658c2caae1cc3b02b75bfc5afb43c54a
Renato Junior <renatojr.dev@gmail.com>
2022-10-26 16:54:49 -0300

    Uso dos métodos com Math

--- stage4/script.js
+++ stage4/script.js
@@ -25,7 +25,6 @@ if (average > 6) {
     alert(`Você está reprovado! Sua média foi ${average}!`)
 }
 
-*/
 
 // GUARDANDO DADOS EM UM ARRAY
 
@@ -33,8 +32,28 @@ let items = []
 
 for (let i = 0; i < 4; i++) {
     let itemName = prompt(`Digite o ${i + 1}º item:`)
-
+    
     items[i] = itemName
 }
 
 alert(items)
+
+*/
+
+// ADIVINHAR UM NÚMERO GERADO ALEATÓRIAMENTE
+
+const randomNumber = Math.round(Math.random() * 10)
+
+let typedNumber = prompt("Acabei de gerar um número entre 1 e 10. Consegue adivinhar qual foi?")
+let numberOfAttempts = 1
+
+while (typedNumber != randomNumber) {
+    typedNumber = prompt("Você errou, tente novamente!")
+    numberOfAttempts++
+}
+
+if (numberOfAttempts == 1) {
+    alert(`Parabéns! Você descobriu que gerei o número ${randomNumber} após ${numberOfAttempts} tentativa. =)`)
+} else {
+    alert(`Parabéns! Você descobriu que gerei o número ${randomNumber} após ${numberOfAttempts} tentativas. =)`)
+}
