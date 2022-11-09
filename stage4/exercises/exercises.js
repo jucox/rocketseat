b1355e0bfa93970bafad0ced2f3c03cfa601ae00
Renato Junior <renatojr.dev@gmail.com>
2022-11-09 17:58:09 -0300

    stage 4 exercise 2

--- stage4/exercises/exercises.js
+++ stage4/exercises/exercises.js
@@ -1,3 +1,5 @@
+/*
+
 // EXERCÍCIO 1
 
 const numberA = Number(prompt("Digite o primeiro número:"))
@@ -18,3 +20,45 @@ alert(`A divisão dos dois números é ${division}`)
 alert(`O resto da divisão dos dois números é ${restOfDivision}`)
 alert(`A soma dos dois números é par: ${evenNumber}`)
 alert(`Os números digitados são iguais: ${sameNumbers}`)
+
+*/
+
+// EXERCÍCIO 2
+
+const students = [
+    {
+        name: "Andrey",
+        firstTestGrade: 10,
+        secondTestGrade: 5
+    },
+    {
+        name: "Guilherme",
+        firstTestGrade: 5,
+        secondTestGrade: 6
+    },
+    {
+        name: "Renato",
+        firstTestGrade: 4,
+        secondTestGrade: 8
+    },
+    {
+        name: "Vitor",
+        firstTestGrade: 8,
+        secondTestGrade: 7
+    }
+]
+
+function averageCalculation(gradeA, gradeB) {
+    const average = (gradeA + gradeB) / 2
+    return average
+}
+
+for (let student of students) {
+    const studentAverage = averageCalculation(student.firstTestGrade, student.secondTestGrade)
+
+    if (studentAverage < 7) {
+        alert(`A média do aluno ${student.name} é: ${studentAverage}\nSentimos muito, você não foi aprovado =(`)
+    } else {
+        alert(`A média do aluno ${student.name} é: ${studentAverage}\nParabéns, você foi aprovado! =)`)
+    }
+}
